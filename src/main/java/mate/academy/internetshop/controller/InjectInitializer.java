d1b493ffbf45ac563bfbee65da36e096c360ff97
tsehelnyk <58398447+tsehelnyk@users.noreply.github.com>
2020-01-14 17:44:53 +0200

    Hw17 log4j (#4)
    * added log4j
    
    * added log4j
    
    * added log4j
    
    * edited log4j.properties
    
    * edited Injector's catch
    
    * Delete log4j-application.log

--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -3,17 +3,21 @@ package mate.academy.internetshop.controller;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import mate.academy.internetshop.lib.Injector;
+import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
+
+    private static final Logger LOGGER = Logger.getLogger(InjectInitializer.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
-            System.out.println("Dependency injection started..");
+            LOGGER.info("Dependency injection started..");
             Injector.injectDependency();
-            System.out.println("and successfully finished!");
+            LOGGER.info("Injection successfully finished");
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            LOGGER.error("Can't inject dependencies: ", e);
+            throw new RuntimeException(e);
         }
     }
 
