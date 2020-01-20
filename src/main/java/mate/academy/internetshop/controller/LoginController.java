7eadf6ff212603ac2083eb0fbb2bd25349235bb3
Tsehelnyk <tsegelnikoff@gmail.com>
2020-01-20 16:26:10 +0200

    deleted commints

--- src/main/java/mate/academy/internetshop/controller/LoginController.java
+++ src/main/java/mate/academy/internetshop/controller/LoginController.java
@@ -35,8 +35,6 @@ public class LoginController extends HttpServlet {
         String password = req.getParameter("psw");
         try {
             User user = userService.login(login, password);
-            Cookie cookie = new Cookie("MATE", user.getToken());
-            resp.addCookie(cookie);
             HttpSession session = req.getSession(true);
             session.setAttribute("user_id", user.getId());
             resp.sendRedirect(req.getContextPath() + "/servlet/index");
